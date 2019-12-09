a573ae0d8b9089d173936a5ad639a9a78f80fc15
Helen Collins-Eromosele <helenokonicha@gmail.com>
2019-12-09 14:42:15 +0000

    initial commit

---
+++ src/first.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { redBright } from 'ansi-colors';
+
+function FirstPage(){
+    
+
+    return(
+        <div className="fPage">
+            <h1>Helen Collins-Eromosele</h1>
+            <p>I am a Front-end web developer</p>
+        </div>
+    )
+}
+
+export default FirstPage;
